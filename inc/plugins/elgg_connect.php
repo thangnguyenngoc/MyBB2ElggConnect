069af2727af0de0afa40585908accb9ffeed7917
Atemix <contactus@atemix.com>
2013-05-29 14:43:35 +0700

    create db hook to global todo: connect with elgg api

--- inc/plugins/elgg_connect.php
+++ inc/plugins/elgg_connect.php
@@ -15,7 +15,7 @@ if(!defined("IN_MYBB"))
 	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
 }
 
-$plugins->add_hook(" member_do_login_end", "elggconnect_login_end");
+$plugins->add_hook(" global_end", "elggconnect_global_end");
 
 function elggconnect_info()
 {
@@ -52,31 +52,45 @@ function elggconnect_info()
  **/
  function elggconnect_install()
  {
+	$db->write_query("
+		CREATE TABLE IF NOT EXISTS ".TABLE_PREFIX."elggconnect_users
+		(`id` int(10) NOT NULL auto_increment,
+		`mbb_uid` int(10) NOT NULL default '0',
+		`mybb_username` varchar(120) NOT NULL DEFAULT '',
+		`elgg_guid` bigint(20) unsigned NOT NULL,
+		`elgg_username` varchar(128) NOT NULL DEFAULT '',
+		`elgg_password` varchar(32) NOT NULL DEFAULT '',
+		`elgg_salt` varchar(8) NOT NULL DEFAULT '',
+		PRIMARY KEY  (id))");
  }
  
 /* * _is_installed():
  *   Called on the plugin management page to establish if a plugin is already installed or not.
  *   This should return TRUE if the plugin is installed (by checking tables, fields etc) or FALSE
  *   if the plugin is not installed.
- *
- * function elggconnect_is_installed()
- * {
- *		global $db;
- *		if($db->table_exists("elggconnect_world"))
- *  	{
- *  		return true;
- *		}
- *		return false;
- * }
- *
- * _uninstall():
+ **/
+ function elggconnect_is_installed()
+ {
+ 	global $db;
+ 	if($db->table_exists("elggconnect_users"))
+  	{
+  		return true;
+ 	}
+ 	return false;
+ }
+ 
+ /** _uninstall():
  *    Called whenever a plugin is to be uninstalled. This should remove ALL traces of the plugin
  *    from the installation (tables etc). If it does not exist, uninstall button is not shown.
- *
- * function elggconnect_uninstall()
- * {
- * }
- *
+ **/
+ function elggconnect_uninstall()
+ {
+	global $db;
+
+	// Drop the Table
+	$db->drop_table("elggconnect_users");
+ }
+ /**
  * _activate():
  *    Called whenever a plugin is activated via the Admin CP. This should essentially make a plugin
  *    "visible" by adding templates/template changes, language changes etc.
@@ -97,10 +111,23 @@ function elggconnect_info()
  */
 
 
-function elggconnect_login_end()
+function elggconnect_global_end()
 {
-	//$userinfo
+	//exit if user not logged in
+	if($mybb->user['uid'] == 0)
+		exit;
 	
+	//check if user already migrated to Elgg
+	$query = $db->simple_select("elggconnect_users", "*", "mbb_uid = '{$mybb->user['uid']}'", array("limit" => 1));
+	$elgg = $db->fetch_field($query);
+	if ($elgg['id'])
+	{
+		//todo: call elgg api to log in
+	}
+	else
+	{
+		//migrate to elgg and update db
+	}
 }
 
 ?>
--- inc/plugins/elgg_connect_readme.txt
+++ inc/plugins/elgg_connect_readme.txt
@@ -3,3 +3,9 @@ call to Elgg API to post activity, wire
 create a table to store username/ password in Mybb plugin then sync this user to Elgg
 authenticate a user from MyBB to Elgg using the stored table
 the password is randomly generated and the user actually doesnt know this password, he only can login to via MyBB before going to Elgg
+
+basic flow:
+user login to mybb
+ElgConnect check if the user already exists in plugin db or not
+if not: create a new elgg user with profile data from Mybb, the generated password is stored in plugin db
+if yes: authenticate the user with Elgg, after that, if user clicks on Elgg site, the user is already authenticated
