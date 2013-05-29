f017f1751d452f5b0fb82c0c330dfc0d0d49b8aa
Atemix <contactus@atemix.com>
2013-05-29 11:25:23 +0700

    initial ElggConnect plugin

---
+++ inc/plugins/elgg_connect.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Elgg Connect Plugin
+ * Copyright 2013 Thang Nguyen, All Rights Reserved
+ *
+ * Website: http://atemix.com
+ * License: LGPL
+ *
+ * 
+ */
+ 
+// Disallow direct access to this file for security reasons
+if(!defined("IN_MYBB"))
+{
+	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.");
+}
+
+$plugins->add_hook(" member_do_login_end", "elggconnect_login_end");
+
+function elggconnect_info()
+{
+	/**
+	 * Array of information about the plugin.
+	 * name: The name of the plugin
+	 * description: Description of what the plugin does
+	 * website: The website the plugin is maintained at (Optional)
+	 * author: The name of the author of the plugin
+	 * authorsite: The URL to the website of the author (Optional)
+	 * version: The version number of the plugin
+	 * guid: Unique ID issued by the MyBB Mods site for version checking
+	 * compatibility: A CSV list of MyBB versions supported. Ex, "121,123", "12*". Wildcards supported.
+	 */
+	return array(
+		"name"			=> "Elgg Connect!",
+		"description"	=> "Connect to Elgg elgg.org",
+		"website"		=> "http://mybb.com",
+		"author"		=> "Thang Nguyen",
+		"authorsite"	=> "http://mybb.com",
+		"version"		=> "1.0",
+		"guid" 			=> "",
+		"compatibility" => "16*"
+	);
+}
+
+/**
+ * ADDITIONAL PLUGIN INSTALL/UNINSTALL ROUTINES
+ *
+ * _install():
+ *   Called whenever a plugin is installed by clicking the "Install" button in the plugin manager.
+ *   If no install routine exists, the install button is not shown and it assumed any work will be
+ *   performed in the _activate() routine.
+ **/
+ function elggconnect_install()
+ {
+ }
+ 
+/* * _is_installed():
+ *   Called on the plugin management page to establish if a plugin is already installed or not.
+ *   This should return TRUE if the plugin is installed (by checking tables, fields etc) or FALSE
+ *   if the plugin is not installed.
+ *
+ * function elggconnect_is_installed()
+ * {
+ *		global $db;
+ *		if($db->table_exists("elggconnect_world"))
+ *  	{
+ *  		return true;
+ *		}
+ *		return false;
+ * }
+ *
+ * _uninstall():
+ *    Called whenever a plugin is to be uninstalled. This should remove ALL traces of the plugin
+ *    from the installation (tables etc). If it does not exist, uninstall button is not shown.
+ *
+ * function elggconnect_uninstall()
+ * {
+ * }
+ *
+ * _activate():
+ *    Called whenever a plugin is activated via the Admin CP. This should essentially make a plugin
+ *    "visible" by adding templates/template changes, language changes etc.
+ *
+ * function elggconnect_activate()
+ * {
+ * }
+ *
+ * _deactivate():
+ *    Called whenever a plugin is deactivated. This should essentially "hide" the plugin from view
+ *    by removing templates/template changes etc. It should not, however, remove any information
+ *    such as tables, fields etc - that should be handled by an _uninstall routine. When a plugin is
+ *    uninstalled, this routine will also be called before _uninstall() if the plugin is active.
+ *
+ * function elggconnect_deactivate()
+ * {
+ * }
+ */
+
+
+function elggconnect_login_end()
+{
+	//$userinfo
+	
+}
+
+?>
---
+++ inc/plugins/elgg_connect_readme.txt
@@ -0,0 +1,5 @@
+write a MyBB plugin to hook into user login, registration, post
+call to Elgg API to post activity, wire
+create a table to store username/ password in Mybb plugin then sync this user to Elgg
+authenticate a user from MyBB to Elgg using the stored table
+the password is randomly generated and the user actually doesnt know this password, he only can login to via MyBB before going to Elgg
